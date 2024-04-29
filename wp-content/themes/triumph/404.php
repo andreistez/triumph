846cb705c79eca9dfda943c8e55819bdbf53a34a
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-04-29 16:01:05 +0300

    initial commit

---
+++ wp-content/themes/triumph/404.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 404 page default template.
+ *
+ * @package WordPress
+ * @subpackage triumph
+ */
+
+get_header();
+?>
+
+<main class="main main-not-found">
+</main>
+
+<?php
+get_footer();
+
