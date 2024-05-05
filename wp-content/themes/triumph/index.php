96bad649b5d2f978be561ffe72822f5c04d6afa7
andrsweb <d3vv.proo@gmail.com>
2024-05-05 08:33:26 +0700

    Created hero section with mobile responsive

--- wp-content/themes/triumph/index.php
+++ wp-content/themes/triumph/index.php
@@ -17,7 +17,58 @@ $sections = carbon_get_the_post_meta( 'page_sections' );
 
 	<main class="main">
 		<section class="hero">
-			HERO!!!!!!!
+			<div class="container">
+				<div class="hero-wrapper">
+					<div class="hero-info">
+						<h1>
+							Клуб <span>акробатического рок-н-ролла</span> в Москве
+							для детей от пяти лет
+						</h1>
+						<div class="hero-items">
+							<div class="hero-item">
+								<div class="hero-item-img">
+									<img src="<?php echo THEME_URI ?>/static/img/hero1.svg" alt="">
+								</div>
+								<p>
+									Редкий и быстро развивающийся спортивный парно-командный стиль танца
+								</p>
+							</div>
+							<div class="hero-item">
+								<div class="hero-item-img">
+									<img src="<?php echo THEME_URI ?>/static/img/hero2.svg" alt="">
+								</div>
+								<p>
+									На сегодня в клубе состоит более 150 человек
+								</p>
+							</div>
+							<div class="hero-item">
+								<div class="hero-item-img">
+									<img src="<?php echo THEME_URI ?>/static/img/hero3.svg" alt="">
+								</div>
+								<p>
+									Хорошая атмосфера, новые друзья и отличные результаты
+								</p>
+							</div>
+							<div class="hero-item">
+								<div class="hero-item-img">
+									<img src="<?php echo THEME_URI ?>/static/img/hero4.svg" alt="">
+								</div>
+								<p>
+									Возможность выполнить звание и стать чемпионом мира
+								</p>
+							</div>
+						</div>
+						<button class="button after-red">
+							Записаться на пробное занятие
+						</button>
+					</div>
+					<div class="hero-video">
+						<video autoplay muted playsinline loop>
+							<source src="<?php echo THEME_URI ?>/static/video/hero.mp4"/>
+						</video>
+					</div>
+				</div>
+			</div>
 		</section>
 
 		<?php
