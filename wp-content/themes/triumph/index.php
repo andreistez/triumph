6c74573f6d006d0a8a6a43f20d92de3f263578fd
andrsweb <d3vv.proo@gmail.com>
2024-05-05 10:24:33 +0700

    Created about section

--- wp-content/themes/triumph/index.php
+++ wp-content/themes/triumph/index.php
@@ -10,6 +10,7 @@
 get_header();
 
 wp_enqueue_style( 'fc-hero', THEME_URI . '/static/css/sections/hero/hero.min.css', [], THEME_VERSION );
+wp_enqueue_style( 'fc-about', THEME_URI . '/static/css/sections/about/about.min.css', [], THEME_VERSION );
 // wp_enqueue_script( 'fc-hero', THEME_URI . '/static/js/hero/hero.min.js', [ 'jquery' ], THEME_VERSION, true );
 
 $sections = carbon_get_the_post_meta( 'page_sections' );
@@ -66,6 +67,63 @@ $sections = carbon_get_the_post_meta( 'page_sections' );
 						<video autoplay muted playsinline loop>
 							<source src="<?php echo THEME_URI ?>/static/video/hero.mp4"/>
 						</video>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="about">
+			<div class="container">
+				<h2>Акробатический рок-н-ролл <span>это</span></h2>
+				<div class="about-items">
+					<div class="about-item">
+						<div class="about-item-inner">
+							<div class="about-item-info">
+								<p>
+									Объединение акробатических элементов и элементы танца. На острие эмоций и в полной атмосфере праздника.
+								</p>
+							</div>
+							<div class="about-item-img">
+								<img src="<?php echo THEME_URI ?>/static/img/about1.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="about-item">
+						<div class="about-item-inner">
+							<div class="about-item-info">
+								<p>
+									Фееричный вид спорта, где на сцене дети показывают высший класс в красивых костюмах.
+								</p>
+							</div>
+							<div class="about-item-img">
+								<img src="<?php echo THEME_URI ?>/static/img/about2.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="about-item">
+						<div class="about-item-inner">
+							<div class="about-item-info">
+								<p>
+									Мальчики и девочки становятся настоящими спортсменами выполняя сложнейшую акробатику в парах.
+								</p>
+							</div>
+							<div class="about-item-img">
+								<img src="<?php echo THEME_URI ?>/static/img/about3.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="about-item">
+						<div class="about-item-inner">
+							<div class="about-item-info">
+								<p>
+									Дети не только учатся танцевать и быть раскрепощёнными,
+									но и учатся правильно взаимодействовать
+									с противоположным полом.
+								</p>
+							</div>
+							<div class="about-item-img">
+								<img src="<?php echo THEME_URI ?>/static/img/about4.png" alt="">
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
