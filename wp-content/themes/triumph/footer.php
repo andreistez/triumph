9a0ef7216949f25ab68f95801abba80ab0e7141a
andrsweb <d3vv.proo@gmail.com>
2024-05-08 12:36:11 +0700

    created footer

--- wp-content/themes/triumph/footer.php
+++ wp-content/themes/triumph/footer.php
@@ -9,6 +9,14 @@
 ?>
 
 			<footer class="footer">
+				<div class="container">
+					<div class="footer-wrapper">
+						<a href="/">
+							<img src="<?php echo THEME_URI ?>/static/img/logo-red.svg" alt="">
+						</a>
+						<p>Клуб акробатического <br> рок-н-ролла в Москве</p>	
+					</div>
+				</div>
 			</footer>
 			<?php wp_footer() ?>
 		</div><!-- .wrapper -->
